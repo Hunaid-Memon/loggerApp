eb6de3e4e25b449d69b704bf15d74ce3ff31e961
Hunaid <61511895+Hunaid-Memon@users.noreply.github.com>
2022-02-11 00:43:41 +0500

    Update addLog using redux

--- src/actions/logAction.js
+++ src/actions/logAction.js
@@ -1,4 +1,4 @@
-import { GET_LOGS, SET_LOADING, LOGS_ERROR } from './types';
+import { GET_LOGS, ADD_LOG , SET_LOADING, LOGS_ERROR } from './types';
 
 // Method No 1
 // export const getLogs = () => {
@@ -17,6 +17,7 @@ import { GET_LOGS, SET_LOADING, LOGS_ERROR } from './types';
 // }
 
 // Method No 2
+// Action to get logs
 export const getLogs = () => async dispatch => {
     try {
         // Set loading to true
@@ -27,6 +28,33 @@ export const getLogs = () => async dispatch => {
         // Dispatch
         dispatch({
             type: GET_LOGS,
+            payload: data
+        })
+    } catch(err) {
+        dispatch({
+            type: LOGS_ERROR,
+            payload: err.response.data
+        })
+    }
+}
+
+// Action to add a log
+export const addLog = log => async dispatch => {
+    try {
+        // Set loading to true
+        setLoading();
+        // Fetch from API
+        const res = await fetch('/logs', {
+            method: 'POST',
+            body: JSON.stringify(log),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        });
+        const data = await res.json();
+        // Dispatch
+        dispatch({
+            type: ADD_LOG,
             payload: data
         })
     } catch(err) {
